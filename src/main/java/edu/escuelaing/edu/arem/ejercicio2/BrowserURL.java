c3382809167c9cd263d48f01684b021f27a04db0
Andres Martin Cantor Urrego <andresmartincantorurrego@MacBook-Pro-de-Andres.local>
2019-09-05 06:21:08 -0500

    Exercise 2 fix

--- src/main/java/edu/escuelaing/edu/arem/ejercicio2/BrowserURL.java
+++ src/main/java/edu/escuelaing/edu/arem/ejercicio2/BrowserURL.java
@@ -18,26 +18,28 @@ public class BrowserURL {
         System.out.println("Browser Exercise # 2 : \n"+
                 "Welcome user !! \n" +
                 "Enter the URL you wish to store: ");
-        this.ReadingURL();
-
-
-    }
-    private void ReadingURL (){
-        BufferedReader readerUrl = new BufferedReader(new InputStreamReader(System.in));
         try {
-            URL url = new URL(readerUrl.readLine());
             FileWriter result = new FileWriter (SAVED_FILE_PATH);
+            URL url = this.readingURL();
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(url.openStream()));
             String line = null ;
             while((line = bufferedReader.readLine()) != null){
                 result.write(line);
             }
             bufferedReader.close();
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        System.out.println(" See in the result path : "+ SAVED_FILE_PATH + "\n" +
+                " The solution of the exercise!!");
+    }
+
+    private URL readingURL () throws IOException {
+        BufferedReader readerUrl = new BufferedReader(new InputStreamReader(System.in));
+        URL url = new URL(readerUrl.readLine());
+        System.out.println("The URL is : " + url);
+        return url;
+
     }
     public static void main(String [] args){
         BrowserURL browserURL = new BrowserURL();
